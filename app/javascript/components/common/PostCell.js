71af5344adbbea2b4901cda41f2a2facb94bff5c
Alex Meyer <acmeyer9@gmail.com>
2018-08-31 13:57:08 -0400

    added action styles

--- app/javascript/components/common/PostCell.js
+++ app/javascript/components/common/PostCell.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppReadMore from './AppReadMore';
+import moment from  'moment';
 
 class PostCell extends React.Component {
   showPost = (e) => {
@@ -42,6 +43,7 @@ class PostCell extends React.Component {
           </div>
           <div className="body mt-2">
             <AppReadMore length={100} text={top_response.content} />
+            <div className="small time-ago text-muted">{moment(top_response.created_at).fromNow()}</div>
           </div>
         </div>
       )
@@ -51,22 +53,25 @@ class PostCell extends React.Component {
   }
 
   renderActionMenu = () => {
+    let {post} = this.props;
     return (
       <div className="cell-actions flex-row d-flex mt-3">
         <div className="action pr-4" onClick={this.toggleUpvote}>
-          <i className="fas fa-arrow-up" />
+          <span className={`action-icon ${post.upvoted ? 'active' : null}`}><i className={`fas fa-arrow-up`} /></span>
+          <span className={`action-count ${post.upvoted ? 'active' : null}`}>{post.upvotes_count > 0 && post.upvotes_count}</span>
         </div>
         <div className="action pr-4" onClick={this.showPostComments}>
-          <i className="far fa-comment" />
+          <span className={`action-icon ${post.responded_to ? 'active' : null}`}><i className={`${post.responded_to ? 'fas' : 'far'} fa-comment`} /></span>
+          <span className={`action-count ${post.responded_to ? 'active' : null}`}>{post.responses_count > 0 && post.responses_count}</span>
         </div>
         <div className="action pr-4" onClick={this.toggleBookmark}>
-          <i className="far fa-bookmark" />
+          <span className={`action-icon ${post.bookmarked ? 'active' : null}`}><i className={`${post.bookmarked ? 'fas' : 'far'} fa-bookmark`} /></span>
         </div>
         <div className="action pr-4" onClick={this.showSharingOptions}>
-          <i className="far fa-share-square" />
+          <span className={`action-icon`}><i className="far fa-share-square" /></span>
         </div>
         <div  className="action" onClick={this.showMoreOptions}>
-          <i className="fas fa-ellipsis-h" />
+          <span className={`action-icon`}><i className="fas fa-ellipsis-h" /></span>
         </div>
       </div>
     )
@@ -77,6 +82,7 @@ class PostCell extends React.Component {
       <div className="card cell post-cell mb-2" onClick={this.showPost}>
         <div className="p-3">
           <AppReadMore length={150} text={this.props.post.content} />
+          <div className="small time-ago text-muted">{moment(this.props.post.created_at).fromNow()}</div>
           {this.renderActionMenu()}
         </div>
         {this.renderTopResponse()}
